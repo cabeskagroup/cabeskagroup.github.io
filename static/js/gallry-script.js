73528bdcf3e0346df957978f1f4ecc66682ff206
cabeskagroup <kisalshehara7@gmail.com>
2023-04-12 12:08:14 +0530

    r

--- static/js/gallry-script.js
+++
@@ -1,7 +0,0 @@
-[...document.querySelectorAll('.column')].map(column => {
-	column.style.setProperty('--animation', 'slide');
-	column.style.setProperty('height', '200%');
-	column.innerHTML = column.innerHTML + column.innerHTML;
-});
-
-
